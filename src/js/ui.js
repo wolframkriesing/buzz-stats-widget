53533f9c0f6b0b8ec1842889d3b4e0aa6a7fb02b
Your Name <nikolai@nikolaionken.com>
2009-09-26 02:39:32 +0200

    - Added console.logs

--- src/js/ui.js
+++ src/js/ui.js
@@ -4,12 +4,14 @@ var ui;
 		onNewTweet:function(tweet, group){
 			// tweet - contains all the data of the latest tweet
 			// group - this is a ref to the group this new tweet is in
+			console.log(arguments);
 		},
 		
 		onNewGroup:function(startTime, group){
 			// If a new group opened.
 			// A new group is
 			// startTime - this is the time when this group starts, i.e. "10:30"
+			console.log(arguments);
 		},
 		
 		colors: [
